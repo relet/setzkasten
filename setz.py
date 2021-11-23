8b6ed7e4b3f2451b2ba7d623223cd1e24257ea1f
Thomas Hirsch <thomas.hirsch@celerway.com>
2021-11-23 21:01:49 +0100

    fix display issues

--- setz.py
+++ setz.py
@@ -13,23 +13,27 @@ target = config.get('target')
 tilesize = config.get('tilesize')
 maxzoom = config.get('maxzoom')
 
+tile_format = '.webp'
+
 # pixel coordinates as x,y
 # tile coordinates as t,u
 
 for source in config.get('sources',[]):
     xrel, yrel, imgzoom = source[1:4]
-    source_im = cv.imread(source[0])
+
+    source_im = cv.imread(source[0], cv.IMREAD_UNCHANGED)
+    #if imgzoom < maxzoom:
+    #    factor = math.pow(2, maxzoom-imgzoom)
+    #    source_im = cv.resize(source_im, (0, 0), fx=factor, fy=factor)
+    # FIXME: memory issues when blowing up - add maxzoom (and minzoom) to define display range
 
     print(source)
 
     zoom = imgzoom
     w = h = 256 # just to pass the first check
 
-    # when scaling down, zoom-1, halve xrel and yrel, w, h
     while zoom > 1 and w > 1 and h > 2:
-        # TODO: scale and tile for all zoom levels
-        # TODO: if zoom < maxzoom, scale up
-        # TODO: if zoom > maxzoom, scale down
+      if zoom <= maxzoom:
 
         # relative zero (center) at the defined zoom level
         x0 = math.floor(tilesize * math.pow(2, zoom-1))
@@ -39,7 +43,7 @@ for source in config.get('sources',[]):
         xi, yi = x0 + xrel, y0 + yrel
 
         # image size
-        # TODO: make w,h,wt,wh depend on zoom level
+        # NOTE: source images should always be transparent png, or overlaps will be covered
         w,h = source_im.shape[:2]
         wt = math.ceil(w / tilesize)
         ht = math.ceil(h / tilesize)
@@ -56,20 +60,25 @@ for source in config.get('sources',[]):
         off_x = xi - xA
         off_y = yi - yA
 
-        # FIXME: adjust range to actually cover the location of the translated image
-        for tx in range(t0, t0+wt):  # try t0-t0+wt
-            for ty in range(u0, u0+ht):
+        off_t = math.floor(off_x / tilesize)
+        off_u = math.floor(off_y / tilesize)
+
+        # CHECK: adjust range to actually cover the location of the translated image
+        for tx in range(0, wt+1):  # TODO: try t0-t0+wt
+            for ty in range(0, ht+1):
                 # read current background tile
                 folder   = target+"/"+str(zoom)+"/"+str(u0+ty)
-                tile_url = folder +"/"+str(t0+tx)+".jpg"
+                tile_url = folder +"/"+str(t0+tx)+tile_format
                 print("Loading "+tile_url)
 
-                white_tile = np.zeros([tilesize, tilesize, 3],dtype=np.uint8)
-                white_tile.fill(255)
+                white_tile = np.zeros([tilesize, tilesize, 4],dtype=np.uint8)
+                #white_tile.fill(255)
 
-                bg = cv.imread(tile_url)
+                bg = cv.imread(tile_url, cv.IMREAD_UNCHANGED)
                 if bg is None:
                     bg = white_tile.copy()
+                bg = cv.cvtColor(bg, cv.COLOR_BGR2BGRA)
+
                 # cut relevant section of source_im
                 from_x = max(0, tx * tilesize - off_x)
                 from_y = max(0, ty * tilesize - off_y)
@@ -82,10 +91,22 @@ for source in config.get('sources',[]):
                 dto_x = dest_x + to_x - from_x
                 dto_y = dest_y + to_y - from_y
 
-                # TODO: treat source image as having white as transparency
-                # LOL: easier fix: use png/webp
                 # paste cutout onto background
-                bg[dest_x:dto_x, dest_y:dto_y] = cutout
+                # TODO: actually paste, not overwrite
+                # eg. overwrite white_tile, then merge with bg
+
+                try:
+                    bg[dest_x:dto_x, dest_y:dto_y] = cutout
+                except:
+                    continue
+                    #print("SOMETHING FAILED")
+                    #cv.imshow('BG',bg)
+                    #print("CUTOUT SIZE:", (from_x, to_x, from_y, to_y))
+                    #print("FROM Y:", (from_y))
+                    #print("TO Y:", (to_y))
+                    #print("H:", h)
+                    #cv.waitKey(1)
+                    #sys.exit(1)
 
                 # then write that tile to file
                 print("Writing ",tile_url)
@@ -95,9 +116,9 @@ for source in config.get('sources',[]):
                     pass
                 cv.imwrite(tile_url, bg)
 
-        zoom = zoom - 1
-        xrel = math.floor(xrel / 2)
-        yrel = math.floor(yrel / 2)
-        source_im = cv.resize(source_im, (0, 0), fx=0.5, fy=0.5)
-        w = math.floor(w / 2)
-        h = math.floor(h / 2)
+      zoom = zoom - 1
+      xrel = math.floor(xrel / 2)
+      yrel = math.floor(yrel / 2)
+      source_im = cv.resize(source_im, (0, 0), fx=0.5, fy=0.5)
+      w = math.floor(w / 2)
+      h = math.floor(h / 2)
